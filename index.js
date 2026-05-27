ca01a3a56ea3209d6f6e90ede82f0d95aec08b9d
Wolleebob <guilherme.schultze.galvao@escola.pr.gov.br>
2026-05-27 10:54:26 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-const express = require("express")
+// npm init
+// npm i express
+// instalar extensão RapidAPI Client
+const express = require('express')
 const app = express()
-const port = 3464
+const port = 3000
+app.use(express.json())
+const fs = require('fs')
+
+app.post("/clientes/cadastro", (req,res) => {
+    const cliente = req.body
+    if (!cliente || Object.keys(cliente).length === 0) {
+        res.status(400).json({resposta: "Body não preenchido"})
+    } else {
+        try {
+            const bd = JSON.parse(fs.readFileSync('bd.json', 'utf8'))
+            bd.push(cliente)
+            fs.writeFileSync('bd.json', JSON.stringify(bd), 'utf8')
+            res.status(200).json({resposta: "Cliente cadastrado com sucesso!"})
+        } catch(error) {
+            res.status(500).json({resposta: error.message})
+        }
+    }    
+})
 
 app.get("/ola", (req, res)=>{
-    res.send("Olá mundo!")
+    res.json({resposta: "Olá Mundo!"})
 })
 
-app.get("/aluno", (req, res)=>{
-    res.json({
-        aluno: "Guilherme",
-        turma: "3DSB"
-    })
-})
-
-app.listen(port, ()=> {
-    console.log("API executando na porta " + port)
+app.listen(port, ()=>{
+    console.log("API rodando na porta " + port)
 })
